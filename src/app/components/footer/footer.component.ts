2a65853f3ad174a36ee7c6a2fd7722d26e403110
jerson <j.antequera@tekneo.es>
2021-07-20 00:59:17 -0500

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  facebookIcon = 'assets/icons/facebook.svg';
+  twitterIcon = 'assets/icons/twitter.svg';
+  instagramIcon = 'assets/icons/instagram.svg';
+  youtubeIcon = 'assets/icons/youtube.svg'
+
   constructor() { }
 
   ngOnInit(): void {
